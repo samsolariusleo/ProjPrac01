2c1dd1633f83b025401f0acad89fc24540994a90
Sam Gan <gan.jingying@dhs.sg>
2013-05-03 17:44:02 +0800

    Fully functional app

--- ProjPrac01.py
+++ ProjPrac01.py
@@ -2,7 +2,7 @@ from flask import *
 from functools import wraps
 import sqlite3
 
-DATABASE = 'sales.db'
+DATABASE = '/home/LionCubFearMe/mysite/sales.db'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -31,15 +31,80 @@ def dashboard():
 @login_required
 def shop():
     g.db = connect_db()
-    cur = g.db.execute('SELECT imgurl, item_name, price, availability FROM items')
-    listofitems = [dict(imgurl = row[0], item_name = row[1], price = row[2], availability = row[3]) for row in cur.fetchall()]
+    cur = g.db.execute('SELECT id, imgurl, item_name, price, availability FROM items')
+    listofitems = [dict(id = row[0], imgurl = row[1], item_name = row[2], price = row[3], availability = row[4])
+                   for row in cur.fetchall()]
     g.db.close()
     return render_template('shop.html', listofitems = listofitems)
+
+@app.route('/buy', methods = ['POST'])
+@login_required
+def buy():
+    item_id = request.form['id']
+    quantity = request.form['quantity']
+    g.db = connect_db()
+    cur = g.db.execute('SELECT id, imgurl, item_name, price FROM items WHERE id ='+ str(item_id))
+    listofdict = [dict(id=row[0], imgurl=row[1], item_name=row[2], price=row[3]) for row in cur.fetchall()]
+    currentorder = listofdict[0]
+    imgurl = currentorder['imgurl']
+    item_name = currentorder['item_name']
+    price = currentorder['price']
+    g.db.execute('INSERT INTO orders (imgurl, item_name, price, quantity) '
+                 'VALUES (?, ?, ?, ?)', [imgurl, item_name, price, quantity])
+    g.db.commit()
+    flash('Your order has been made')
+    return redirect(url_for('shop'))
+
+@app.route('/confirm')
+@login_required
+def confirm():
+    g.db = connect_db()
+    cur = g.db.execute('SELECT id, imgurl, item_name, price, quantity FROM orders')
+    listorders = [dict(id=row[0], imgurl=row[1], item_name=row[2], price=row[3], quantity=row[4]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('confirm.html', listorders = listorders)
+
+@app.route('/delete/<int:id>')
+@login_required
+def delete(id):
+    g.db = connect_db()
+    cur = g.db.execute('DELETE FROM orders WHERE id='+str(id))
+    g.db.commit()
+    g.db.close()
+    flash('The item has been removed from your cart')
+    return redirect(url_for('confirm'))
+
+@app.route('/sendorders', methods=['POST'])
+@login_required
+def sendorders():
+    g.db = connect_db()
+    # get all information about current order and send it over to another database
+    confirmed = g.db.execute('SELECT * FROM orders')
+    listoforders = [dict(id=row[0], imgurl=row[1], item_name=row[2], price=row[3], quantity=row[4])
+                    for row in confirmed.fetchall()]
+    for i in range(0,len(listoforders)):
+        currentorder = listoforders[i]
+        imgurl = currentorder['imgurl']
+        item_name = currentorder['item_name']
+        price = currentorder['price']
+        quantity = currentorder['quantity']
+        g.db.execute('INSERT INTO confirmed (imgurl, item_name, price, quantity) '
+                     'VALUES (?, ?, ?, ?)', [imgurl, item_name, price, quantity])
+        g.db.commit()
+    cur = g.db.execute('DELETE FROM orders')
+    g.db.commit()
+    flash('Your order has been confirmed and submitted for processing.')
+    return redirect(url_for('history'))
 
 @app.route('/history')
 @login_required
 def history():
-    return render_template('history.html')
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * FROM confirmed')
+    confirmedlist = [dict(id=row[0], imgurl=row[1], item_name=row[2], price=row[3], quantity=row[4])
+                     for row in cur.fetchall()]
+    g.db.close()
+    return render_template('history.html', confirmedlist = confirmedlist)
 
 @app.route('/logout')
 def logout():
---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created with PyCharm.
+ * User: user
+ * Date: 5/3/13
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// BACK ONE PAGE
+
+function goBack()
+        {
+            window.history.go(-1)
+        }
